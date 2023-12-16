219c4d05c3ae97fe0492fec878fd933bf9173e1a
Yael Balbastre <ybalbastre@mgh.harvard.edu>
2023-12-16 18:11:11 -0500

    Remove python impl now that it has its own branch

--- python/niizarr/__init__.py
+++
@@ -1,2 +0,0 @@
-from ._nii2zarr import nii2zarr, nii2json   # noqa: F401
-from ._zarr2nii import zarr2nii, bin2nii    # noqa: F401
--- python/setup.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from setuptools import setup, find_packages
-
-setup(packages=find_packages())
